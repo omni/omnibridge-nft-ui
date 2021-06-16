b45278e2472b9d9a1f09ac258799cb234a47d435
dan13ram <dan13ram@gmail.com>
2021-06-16 22:08:47 +0530

    refined TokenDisplay component

--- packages/dapp/src/components/bridge/TokenDisplay.jsx
+++ packages/dapp/src/components/bridge/TokenDisplay.jsx
@@ -1,27 +1,56 @@
 import { CheckIcon } from '@chakra-ui/icons';
-import { Flex, useBoolean } from '@chakra-ui/react';
+import { Flex, Link, Text, useBoolean, VStack } from '@chakra-ui/react';
+import { TopRightArrowIcon } from 'icons/TopRightArrowIcon';
+import { getAccountString, getExplorerUrl } from 'lib/helpers';
 import { fetchImageUri } from 'lib/uriHelpers';
 import React, { useEffect, useState } from 'react';
 
-const Checkbox = ({ isChecked = false }) => (
+const Checkbox = ({ isChecked = false, ...props }) => (
   <Flex
-    w="5"
-    h="5"
+    w="1.125rem"
+    h="1.125rem"
     bg="blackAlpha.800"
     justify="center"
     align="center"
     borderRadius="0.25rem"
+    {...props}
   >
-    {isChecked && <CheckIcon color="white" boxSize="0.7rem" />}
+    {isChecked && <CheckIcon color="white" boxSize="0.625rem" />}
   </Flex>
 );
 
-export const ERC721TokenDisplay = ({ token, isDisabled = false }) => {
-  const { imageUri } = token;
+const TokenTag = ({ children, showArrow = false, ...props }) => (
+  <Flex
+    bg="blackAlpha.800"
+    color="white"
+    align="center"
+    justify="center"
+    borderRadius="full"
+    px="2"
+    fontSize="0.65rem"
+    h="1.125rem"
+    {...props}
+  >
+    <Text textAlign="center" verticalAlign="center" lineHeight="0.875rem">
+      {children}
+    </Text>
+    {showArrow && (
+      <Flex h="0.6rem" direction="column" align="flex-end" w="0.65rem">
+        <TopRightArrowIcon color="white" boxSize="0.45rem" />
+      </Flex>
+    )}
+  </Flex>
+);
+
+export const ERC721TokenDisplay = ({
+  token,
+  isDisabled = false,
+  disableCheckbox = false,
+}) => {
+  const { chainId, imageUri, tokenId, address } = token;
   const [isChecked, { toggle: toggleCheck }] = useBoolean(false);
   return (
     <Flex
-      as="button"
       w="7.5rem"
       h="7.5rem"
       p="0.5rem"
@@ -30,11 +59,9 @@ export const ERC721TokenDisplay = ({ token, isDisabled = false }) => {
       borderRadius="0.375rem"
       overflow="hidden"
       bgColor="white"
-      onClick={isDisabled ? undefined : toggleCheck}
       position="relative"
       opacity={isDisabled ? 0.5 : 1}
       disabled={isDisabled}
-      cursor={isDisabled ? 'not-allowed' : 'pointer'}
     >
       <Flex
         bgColor="white"
@@ -48,90 +75,142 @@ export const ERC721TokenDisplay = ({ token, isDisabled = false }) => {
         h="100%"
         p="1"
         direction="column"
+        justify="space-between"
+        role="group"
       >
-        {isDisabled ? null : (
-          <Flex justify="flex-end">
-            <Checkbox isChecked={isChecked} />
-          </Flex>
-        )}
+        <Flex justify="flex-end">
+          {isDisabled || disableCheckbox ? null : (
+            <Checkbox
+              isChecked={isChecked}
+              onClick={toggleCheck}
+              cursor="pointer"
+            />
+          )}
+        </Flex>
+        <VStack
+          spacing="0.3125rem"
+          w="100%"
+          align="flex-start"
+          visibility="hidden"
+          opacity={0}
+          transition="all 0.25s"
+          _groupHover={{ visibility: 'visible', opacity: 1 }}
+        >
+          <TokenTag>EIP-1155</TokenTag>
+          <TokenTag>{`ID: ${tokenId}`}</TokenTag>
+          <Link
+            href={`${getExplorerUrl(chainId)}/address/${address}`}
+            isExternal
+          >
+            <TokenTag cursor="pointer" showArrow>
+              {getAccountString(address)}
+            </TokenTag>
+          </Link>
+        </VStack>
       </Flex>
-      <Flex
-        w="100%"
-        h="100%"
-        position="absolute"
-        top="0"
-        left="0"
-        transition="background 0.25s"
-        _hover={isDisabled ? {} : { bg: 'blackAlpha.200' }}
-      />
     </Flex>
   );
 };
 
-export const ERC1155TokenDisplay = ({ token, isDisabled = false }) => {
-  const { imageUri, amount = 10 } = token;
+export const ERC1155TokenDisplay = ({
+  token,
+  isDisabled = false,
+  disableCheckbox = false,
+}) => {
+  const { chainId, imageUri, tokenId, amount, address } = token;
   const [isChecked, { toggle: toggleCheck }] = useBoolean(false);
   return (
     <Flex
-      as="button"
       w="7.5rem"
       h="7.5rem"
-      p="0.5rem"
       justify="center"
       align="center"
-      borderRadius="0.375rem"
-      overflow="hidden"
-      bgColor="white"
-      onClick={isDisabled ? undefined : toggleCheck}
       position="relative"
-      opacity={isDisabled ? 0.5 : 1}
-      disabled={isDisabled}
-      cursor={isDisabled ? 'not-allowed' : 'pointer'}
     >
       <Flex
-        bgColor="white"
-        bgImage={imageUri ? `url(${imageUri})` : undefined}
-        bgPos="center"
-        bgSize="contain"
-        bgRepeat="no-repeat"
-        borderRadius="0.375rem"
-        overflow="hidden"
-        w="100%"
-        h="100%"
-        p="1"
-        direction="column"
-      >
-        <Flex justify="space-between">
-          <Flex
-            bg="blackAlpha.800"
-            color="white"
-            align="center"
-            justify="center"
-            borderRadius="full"
-            px="2"
-            fontSize="0.65rem"
-            h="1.25rem"
-          >
-            {`x${amount}`}
-          </Flex>
-          {isDisabled ? null : <Checkbox isChecked={isChecked} />}
-        </Flex>
-      </Flex>
-      <Flex
-        w="100%"
-        h="100%"
+        w="calc(100% - 1.25rem)"
+        h="calc(100% + 0.625rem)"
         position="absolute"
         top="0"
-        left="0"
-        transition="background 0.25s"
-        _hover={isDisabled ? {} : { bg: 'blackAlpha.200' }}
+        left="0.625rem"
+        borderRadius="0.375rem"
+        background="linear-gradient(180deg, rgba(255, 255, 255, 0) 96.55%, #FFFFFF 100%)"
       />
+      <Flex
+        w="calc(100% - 0.625rem)"
+        h="calc(100% + 0.3125rem)"
+        position="absolute"
+        top="0"
+        left="0.3125rem"
+        borderRadius="0.375rem"
+        background="linear-gradient(180deg, rgba(255, 255, 255, 0) 96.43%, #FFFFFF 100%)"
+      />
+      <Flex
+        w="7.5rem"
+        h="7.5rem"
+        p="0.5rem"
+        justify="center"
+        align="center"
+        borderRadius="0.375rem"
+        overflow="hidden"
+        bgColor="white"
+        position="relative"
+        opacity={isDisabled ? 0.5 : 1}
+        disabled={isDisabled}
+      >
+        <Flex
+          bgColor="white"
+          bgImage={imageUri ? `url(${imageUri})` : undefined}
+          bgPos="center"
+          bgSize="contain"
+          bgRepeat="no-repeat"
+          borderRadius="0.375rem"
+          overflow="hidden"
+          w="100%"
+          h="100%"
+          p="1"
+          direction="column"
+          justify="space-between"
+          role="group"
+        >
+          <Flex justify="space-between">
+            <TokenTag>{`x${amount}`}</TokenTag>
+            {isDisabled || disableCheckbox ? null : (
+              <Checkbox
+                isChecked={isChecked}
+                onClick={toggleCheck}
+                cursor="pointer"
+              />
+            )}
+          </Flex>
+          <VStack
+            spacing="0.3125rem"
+            w="100%"
+            align="flex-start"
+            visibility="hidden"
+            opacity={0}
+            transition="all 0.25s"
+            _groupHover={{ visibility: 'visible', opacity: 1 }}
+          >
+            <TokenTag>EIP-1155</TokenTag>
+            <TokenTag>{`ID: ${tokenId}`}</TokenTag>
+            <Link
+              href={`${getExplorerUrl(chainId)}/address/${address}`}
+              isExternal
+            >
+              <TokenTag cursor="pointer" showArrow>
+                {getAccountString(address)}
+              </TokenTag>
+            </Link>
+          </VStack>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
 
 export const TokenDisplay = ({ token: inputToken }) => {
-  const { type, uri } = inputToken;
+  const { is1155, uri } = inputToken;
 
   const [imageUri, setImageUri] = useState(uri);
 
@@ -141,7 +220,7 @@ export const TokenDisplay = ({ token: inputToken }) => {
 
   const token = { ...inputToken, imageUri };
 
-  return type === 'erc-1155' ? (
+  return is1155 ? (
     <ERC1155TokenDisplay token={token} />
   ) : (
     <ERC721TokenDisplay token={token} />
---
+++ packages/dapp/src/icons/TopRightArrowIcon.jsx
@@ -0,0 +1,14 @@
+import { createIcon } from '@chakra-ui/icon';
+import * as React from 'react';
+
+export const TopRightArrowIcon = createIcon({
+  displayName: 'TopRightArrowIcon',
+  path: (
+    <path
+      d="M0.5 1H5.5M5.5 1L0.5 6M5.5 1V6"
+      stroke="currentColor"
+      strokeLinecap="round"
+    />
+  ),
+  viewBox: '0 0 6 7',
+});
