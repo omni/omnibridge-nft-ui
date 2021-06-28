add34980a05ea8d7bef56f18dd468f0add249ccd
dan13ram <dan13ram@gmail.com>
2021-06-28 10:38:03 +0530

    better ux for selecting erc 1155

--- packages/dapp/src/components/common/ERC1155TokenDisplay.jsx
+++ packages/dapp/src/components/common/ERC1155TokenDisplay.jsx
@@ -42,12 +42,17 @@ export const ERC1155TokenDisplay = ({
   const onCheck = useCallback(() => {
     if (disableCheckbox) return;
     if (!isChecked) {
-      onOpen();
+      if (amount.toString() === `1`) {
+        selectToken({ ...token, amount: amount.toString() });
+        setChecked(true);
+      } else {
+        onOpen();
+      }
     } else {
       unselectToken(token);
       setChecked(false);
     }
-  }, [disableCheckbox, isChecked, unselectToken, token, onOpen]);
+  }, [disableCheckbox, isChecked, unselectToken, token, onOpen, amount]);
 
   useEffect(() => {
     if (disableCheckbox) return;
@@ -171,7 +176,7 @@ export const SelectEIP1155TokenModal = ({
   setChecked,
   token,
 }) => {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState(1);
   const onClick = useCallback(() => {
     selectToken({ ...token, amount: amount.toString() });
     setChecked(true);
